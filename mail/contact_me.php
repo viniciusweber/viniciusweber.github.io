ad7858746c6c42e010705188781053a13d673e68
Vinicius Weber <vinicius.weber@hotmail.com>
2018-07-25 13:24:26 -0300

    contactar

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,9 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['telefone']) || empty($_POST['mensagem']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
-}
+
 
 $nome = strip_tags(htmlspecialchars($_POST['nome']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
@@ -14,7 +11,7 @@ $mensagem = strip_tags(htmlspecialchars($_POST['mensagem']));
 $to = "vinicius.weber@hotmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "GiT Portfolio formulario:  $nome";
 $body = "A mensagem a seguir foi enviada pela página do GIT através do formulário de contato do Portfolio.\n\n"."Dados preenchidos:\n\nNome: $nome\n\nEmail: $email\n\nTelefone: $telefone\n\nMensagem:\n$mensagem";
-$header = "From: vinicius.weber@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: vinicius.weber2@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
