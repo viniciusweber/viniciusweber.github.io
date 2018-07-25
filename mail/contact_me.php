8ad16e8aee09e1a7a3d0d588f7f195523d41abf3
Vinicius Weber <vinicius.weber@hotmail.com>
2018-07-25 13:51:35 -0300

    cont

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,9 @@
 <?php
 // Check for empty fields
-
+if(empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['telefone']) || empty($_POST['mensagem']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
 
 $nome = strip_tags(htmlspecialchars($_POST['nome']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
@@ -13,6 +16,6 @@ $subject = "GiT Portfolio formulario:  $nome";
 $body = "A mensagem a seguir foi enviada pela página do GIT através do formulário de contato do Portfolio.\n\n"."Dados preenchidos:\n\nNome: $nome\n\nEmail: $email\n\nTelefone: $telefone\n\nMensagem:\n$mensagem";
 $header = "$email"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
-mail($to, $subject, $body, $header);
-
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
 ?>
