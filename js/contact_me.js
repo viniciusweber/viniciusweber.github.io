da342b3eb00d10937976a5dc4a37ff40516f14d4
Vinicius Weber <vinicius.weber@hotmail.com>
2018-07-25 13:02:20 -0300

    contsto
    contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -46,7 +46,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Desculpa " + firstName + ", O servidor não está respondendo. Por favor tente mais tarde!"));
+          $('#success > .alert-danger').append($("<strong>").text("Desculpa " + firstName + email + telefone + mensagem + firstName + ", O servidor não está respondendo. Por favor tente mais tarde!"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
